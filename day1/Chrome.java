6084be643682f7325d7af7fbfb04efdd1279792a
maggie280 <93188795+maggie280@users.noreply.github.com>
2025-07-09 18:23:34 +0530

    Add files via upload
    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version = 91.f;
+		String developer = "Google";
+		boolean beta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("My version is : "  + version);
+		System.out.println("Developed by : " + developer);
+		System.out.println("Beta type is : " + beta);
+		System.out.println("Released year " + releaseYear);
+		System.out.println("Shorcut used : " + shortcutKey);
+
+	}
+
+}
